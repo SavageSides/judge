68b214b8e68ae117cbf348b08b89e87f583f9109
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-09-15 13:50:53 -0500

    Update judgecodes.py

--- judgecodes.py
+++ judgecodes.py
@@ -203,7 +203,7 @@ async def clear(ctx, amount=None):
 async def help(ctx):
     author = ctx.message.author
     embed = discord.Embed(color0xff00e6)
-    set_author(name='Helping and ready to provide!', icon_url=client.user.default_avatar_url)
+    embed.set_author(name='Helping and ready to provide!', icon_url=client.user.default_avatar_url)
     embed.add_field(name='Reaction Legend:', value=':timer: ``Time Category`` \n :tada: ``Fun Commands`` \n :lock: ``Administration`` \n :tools: ``Moderation`` \n :ping_pong: ``Utility``', inline=False)
     msg = await client.send_message(author, embed=embed)
     await client.add_reaction(msg, "\U000023f2")
